2ab23299a10528a98492a6b9224ec3aa7f85a460
Taiwo Arowoyele <tarowoyele@gmail.com>
2024-09-26 10:31:44 -0400

    Created Vpc networking

--- backend1.tf
+++ backend1.tf
@@ -2,8 +2,8 @@
 terraform {
   backend "s3" {
     bucket         = "taiwterraform1"
-    key            = "taiwo-app/terraform.tfstate"
+    key            = "path/to/terraform.tfstate"
     region         = "us-east-2"
-    dynamodb_table = "terraform-state-lock2"
+    dynamodb_table = "terraform_lock"
   }
 }
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-provider "aws"{
-    region = "us-east-2"
+provider "aws" {
+  region = "us-east-2"
 }
 
 
